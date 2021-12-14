8e4b993146dd9d249665778c78154792fa128090
RadJester41 <40651995+hugenpwe17@users.noreply.github.com>
2021-12-15 01:35:59 +0800

    modify format

--- rtl/ADC/ADC.sv
+++ rtl/ADC/ADC.sv
@@ -4,49 +4,48 @@ Driver for AD7928 ADC on DE1_SoC board.
 See accompanying tutorial document for details.
 */
 
-module ADC
-
+module ADC #(
 	// parameters
 	// range (0: 0V-5V, 1: 0V-2.5V)
 	// coding (0: two's complement, 1: straight binary)
-	#(parameter CODING = 1'b1, parameter RANGE = 1'b0)
-	
-	// inputs and outputs
-	(clock, reset, ADC_CS_N, ADC_DIN, ADC_SCLK, ADC_DOUT,data);
-
-	// interface inputs / outputs
-	input clock, reset;          // clock (50MHz) and reset
-	output reg [7:0][11:0] data; // ADC data out, 8 channels, 12 bits per channel
-	// connect to top level pins
-	output reg ADC_CS_N;         // ADC chip selection
-	output reg ADC_DIN;          // ADC serial data in (to ADC)
-	output reg ADC_SCLK;         // ADC serial clock
-	input ADC_DOUT;              // ADC serial data out (from ADC)
+	parameter CODING = 1'b1, parameter RANGE = 1'b0
+	) (
+		// interface inputs / outputs
+		input clock, 				// clock (50MHz)
+		input reset, 				// reset singal
+		
+		output reg [7:0][11:0] data,	// ADC data out, 8 channels, 12 bits per channel
+		// connect to top level pins
+		output reg ADC_CS_N,		// ADC chip selection singal
+		output reg ADC_DIN,			// ADC serial data in (to ADC)
+		output reg ADC_SCLK,		// ADC serial clock
+		input ADC_DOUT				// ADC serial data out (from ADC)
+);
 
 	// states
 	parameter QUIET0 = 3'b000, QUIET1 = 3'b001, QUIET2 = 3'b010;
 	parameter CYCLE0 = 3'b100, CYCLE1 = 3'b101, CYCLE2 = 3'b110, CYCLE3 = 3'b111;
 	
 	// internal state holding elements
-	reg [2:0] state;   // present state
-	reg [2:0]addr;    // present channel address
-	reg [3:0] count;   // present count
-	reg [14:0] buffer; // present buffer contents
+	reg [2:0] 	state;   	// present state
+	reg [2:0]	addr;    	// present channel address
+	reg [3:0] 	count;   	// present count
+	reg [14:0] 	buffer; 	// present buffer contents
 	
 	// initial values
 	initial begin
-		ADC_CS_N <= 1'b1;
-		ADC_DIN <= 1'b0;
-		ADC_SCLK <= 1'b1;
-		state <= QUIET0;
-		addr <= 3'b0;
-		count <= 4'b0;
-		buffer <= 15'b0;
+		ADC_CS_N 	<= 		1'b1;
+		ADC_DIN 	<= 		1'b0;
+		ADC_SCLK 	<= 		1'b1;
+		state 		<= 		QUIET0;
+		addr 		<= 		3'b0;
+		count 		<= 		4'b0;
+		buffer 		<= 		15'b0;
 	end
 	
 	// intermediate values
-	wire [3:0] count_incr; // count + 1    
-	reg ctrl;              // present control bit
+	wire [3:0] 	count_incr; 		// count + 1    
+	reg 		ctrl;              	// present control bit
 	
 	// determine count_incr
 	assign count_incr = count + 1'b1;
@@ -86,13 +85,13 @@ module ADC
 	always @(posedge clock)
 		if (reset)
 			begin
-				ADC_CS_N <= 1'b1;
-				ADC_DIN <= 1'b0;
-				ADC_SCLK <= 1'b1;
-				state <= QUIET0;
-				addr <= 3'b0;
-				count <= 4'b0;
-				buffer <= 15'b0;
+				ADC_CS_N 	<= 		1'b1;
+				ADC_DIN 	<= 		1'b0;
+				ADC_SCLK 	<= 		1'b1;
+				state 		<= 		QUIET0;
+				addr 		<= 		3'b0;
+				count 		<= 		4'b0;
+				buffer 		<= 		15'b0;
 			end
 		else
 			begin
