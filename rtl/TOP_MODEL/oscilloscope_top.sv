163a7019797130e224cb584093f8c7e982868350
RadJester41 <40651995+hugenpwe17@users.noreply.github.com>
2021-12-27 19:48:59 +0800

    modify format

--- rtl/TOP_MODEL/oscilloscope_top.sv
+++ rtl/TOP_MODEL/oscilloscope_top.sv
@@ -1,42 +1,28 @@
 module oscilloscope(
-CLK,
-DCAC,
-reset,
-VGA_hSync, 
-VGA_vSync, 
-VGA_clk,
-blank_n,
-Red,
-Blue,
-Green,
-ADC_DIN,
-ADC_CS_N,
-ADC_DOUT,
-ADC_SCLK,
-button_d,
-button_u
-);
+	input wire CLK,
+	input wire DCAC,
+	input wire reset,
+	
+	output wire	VGA_hSync, 
+	output wire	VGA_vSync, 
+	output wire	VGA_clk,
+	output wire	blank_n,
+	output wire [7:0]	Red,
+	output wire [7:0]	Blue,
+	output wire [7:0]	Green,
 
-input wire DCAC;
-input CLK;
-input ADC_DOUT;
-input reset;
-input button_d;
-input button_u;
+	output wire ADC_DIN,
+	output wire ADC_CS_N,
+	input wire	ADC_DOUT,
+	output wire ADC_SCLK,
+
+	input wire	button_d,
+	input wire	button_u
+);
 
 //reg [11:0]TRIG = 12'd2048;
 //input SCALE;
 reg [2:0]SCALE = 3'd4;
-output VGA_clk;
-output VGA_hSync;
-output VGA_vSync;
-output blank_n;
-output wire [7:0]Red;
-output wire [7:0]Blue;
-output wire [7:0]Green;
-output ADC_DIN;
-output ADC_CS_N;
-output ADC_SCLK;
 
 wire [11:0]Readed_data;
 wire ADC_CLK;
@@ -66,7 +52,6 @@ wire [7:0]rgb_text;
 wire [7:0]display;
 wire waveform;
 wire [24:0]DATA_OUT;
-wire [9:0]data_out;
 wire Red_e;
 //wire [7:0]dataout;
 wire Blue_e;
@@ -75,16 +60,16 @@ wire [24:0]MEAN_OUT;
 wire [9:0]mean_out;
 wire [24:0]TRIG_OUT;
 wire [9:0]trig_out;
+wire [9:0]data_out;
 wire red_2;
 wire [7:0]RED_2;
-wire [7:0]RED_E;
 //assign 
+wire [7:0]RED_E;
 //assign TRIG = mean;
 assign mean_out = MEAN_OUT[9:0];
 assign trig_out = TRIG_OUT[9:0];
 //assign Blue = (waveform&(VGA_out[11:4]&yCount));
 ADCmodule ADC(
-	.clock(CLK),			// clock(50MHz)
 	.reset(reset),			// reset
 
 	.ADC_CS_N(ADC_CS_N),    // ADC chip selection
@@ -94,6 +79,7 @@ ADCmodule ADC(
 	.ADC_CLK(ADC_CLK),		
 	.Readed_data1(Readed_data)		// output digit data()
 );
+	.clock(CLK),			//  clock(50MHz)
 
 font_gen fon(
 	.clock(CLK),
